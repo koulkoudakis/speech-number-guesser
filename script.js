f537f9e2bf181abd124111b7c5ccb3427fe87a93
koulkoudakis <koulkoudakis@gmail.com>
2021-07-04 13:30:00 -0500

    Updated html, js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-const msgEl = document.getElementById('msg');
+const msgEl = document.getElementById("msg");
 
 const randomNum = getRandomNumber();
 
-console.log('Number:', randomNum);
+console.log("Number:", randomNum);
 
-window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
-
+window.SpeechRecognition =
+  window.SpeechRecognition || window.webkitSpeechRecognition;
 
 let recognition = new window.SpeechRecognition();
 
@@ -19,7 +19,6 @@ function onSpeak(e) {
   writeMessage(msg);
   checkNumber(msg);
 }
-
 
 // Write user speech
 function writeMessage(msg) {
@@ -35,13 +34,13 @@ function checkNumber(msg) {
 
   // Check number validity
   if (Number.isNaN(num)) {
-    msg.innerHTML += '<div>That is not a valid number</div>'
+    msg.innerHTML += "<div>That is not a valid number</div>";
     return;
   }
 
   // Check number is within range
   if (num > 100 || num < 1) {
-    msgEl.innerHTML += '<div>Number must be between 1 and 100</div>'
+    msgEl.innerHTML += "<div>Number must be between 1 and 100</div>";
     return;
   }
 
@@ -50,11 +49,11 @@ function checkNumber(msg) {
     document.body.innerHTML = `
     <h2>Congratulation! You guessed the number! <br><br> it was ${num}</h2>
     <button class="play-again" id="play-again">Play again</button>
-    `
+    `;
   } else if (num > randomNum) {
-    msgEl.innerHTML += '<div>LOWER!</div>'
+    msgEl.innerHTML += "<div>LOWER!</div>";
   } else {
-    msgEl.innerHTML += '<div>HIGHER!</div>'
+    msgEl.innerHTML += "<div>HIGHER!</div>";
   }
 }
 
@@ -64,94 +63,15 @@ function getRandomNumber() {
 
 // Speak result
 
-recognition.addEventListener('result', onSpeak);
+recognition.addEventListener("result", onSpeak);
 
 // End speech recognition service
-recognition.addEventListener('end', () => recognition.start());
+recognition.addEventListener("end", () => recognition.start());
 
 // Play again event
 
-document.body.addEventListener('click', e => {
-  if (e.target.id == 'play-again') {
-  window.location.reload();
-  }
-
-})
-
-/*
-const msgEl = document.getElementById('msg');
-
-const randomNum = getRandomNumber();
-
-console.log('Number:', randomNum);
-
-window.SpeechRecognition =
-  window.SpeechRecognition || window.webkitSpeechRecognition;
-
-let recognition = new window.SpeechRecognition();
-
-// Start recognition and game
-recognition.start();
-
-// Capture user speak
-function onSpeak(e) {
-  const msg = e.results[0][0].transcript;
-
-  writeMessage(msg);
-  checkNumber(msg);
-}
-
-// Write what user speaks
-function writeMessage(msg) {
-  msgEl.innerHTML = `
-    <div>You said: </div>
-    <span class="box">${msg}</span>
-  `;
-}
-
-// Check msg against number
-function checkNumber(msg) {
-  const num = +msg;
-
-  // Check if valid number
-  if (Number.isNaN(num)) {
-    msgEl.innerHTML += '<div>That is not a valid number</div>';
-    return;
-  }
-
-  // Check in range
-  if (num > 100 || num < 1) {
-    msgEl.innerHTML += '<div>Number must be between 1 and 100</div>';
-    return;
-  }
-
-  // Check number
-  if (num === randomNum) {
-    document.body.innerHTML = `
-      <h2>Congrats! You have guessed the number! <br><br>
-      It was ${num}</h2>
-      <button class="play-again" id="play-again">Play Again</button>
-    `;
-  } else if (num > randomNum) {
-    msgEl.innerHTML += '<div>GO LOWER</div>';
-  } else {
-    msgEl.innerHTML += '<div>GO HIGHER</div>';
-  }
-}
-
-// Generate random number
-function getRandomNumber() {
-  return Math.floor(Math.random() * 100) + 1;
-}
-
-// Speak result
-recognition.addEventListener('result', onSpeak);
-
-// End SR service
-recognition.addEventListener('end', () => recognition.start());
-
-document.body.addEventListener('click', e => {
-  if (e.target.id == 'play-again') {
+document.body.addEventListener("click", (e) => {
+  if (e.target.id == "play-again") {
     window.location.reload();
   }
 });
